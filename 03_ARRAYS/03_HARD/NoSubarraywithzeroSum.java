371139e39059478bd5fef4694476d69ec0dfcc10
Chandrasish-Bhattacharya <chandrasish3456@gmail.com>
2025-12-25 00:01:48 +0530

    Initial Commit

--- 03_ARRAYS/03_HARD/NoSubarraywithzeroSum.java
+++ 03_ARRAYS/03_HARD/NoSubarraywithzeroSum.java
@@ -23,6 +23,8 @@
 // - {-2, 2, -8, 1, 7}
 // - {6, -2, 2, -8, 1, 7, 4, -10}
 // The length of the longest subarray with sum zero is 8.
+import java.util.HashMap;
+
 public class NoSubarraywithzeroSum {
     public static int mysol(int N , int[] nums){
         int maxLength = 0;
@@ -37,9 +39,30 @@ public class NoSubarraywithzeroSum {
         }
         return maxLength + 1;
     }
+
+    public static int StriversSol(int N , int[] nums){
+        
+        HashMap<Integer , Integer> mpp = new HashMap<>();
+        int maxi = 0; 
+        int sum = 0;
+        for(int i = 0 ; i < N ; i++){
+            sum += nums[i];
+            if(sum == 0){
+                maxi = i + 1;
+            } else{
+                if(mpp.get(sum) != null){
+                    maxi = Math.max(maxi , i - mpp.get(sum));
+                }
+                else{
+                    mpp.put(sum , i);
+                }
+            }
+        }
+        return maxi;
+    }
     public static void main(String[] args){
         int[] nums = {6, -2, 2, -8, 1, 7, 4, -10};
         int N = nums.length;
-        System.out.println(mysol(N, nums));
+        System.out.println(StriversSol(N, nums));
     }
 }
